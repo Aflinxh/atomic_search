e3e3d0e4cdf58fe3551e993b26554efd23b4e11d
alfinalamsyahhh <alfinalamsyahhh@gmail.com>
2024-10-07 16:53:18 +0000

    Update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,5 @@
 import pytest
 import os
-import json
-import datetime as dt
-import pytz
-
 
 @pytest.fixture
 def min_atom_size():
@@ -47,26 +43,3 @@ def log_dir(request):
     os.makedirs(log_dir, exist_ok=True)
     
     return log_dir
-
-# Update the save_test_logs function to handle differences
-def save_test_logs(test_name, log_dir, filtered_result, filtered_expected, differences):
-    jakarta_tz = pytz.timezone('Asia/Jakarta')
-
-    # Create the folder structure for logs
-    time_stamp = dt.datetime.now(jakarta_tz).strftime("%Y-%m-%d_%H-%M-%S")
-    test_log_dir = os.path.join(log_dir, f"{test_name}/{time_stamp}")
-    os.makedirs(test_log_dir, exist_ok=True)
-
-    # Write filtered result, expected and differences to separate log files
-    result_file = os.path.join(test_log_dir, 'filtered_result.json')
-    expected_file = os.path.join(test_log_dir, 'filtered_expected.json')
-    differences_file = os.path.join(test_log_dir, 'differences.json')
-
-    with open(result_file, 'w') as f:
-        json.dump(filtered_result, f, indent=4)
-    with open(expected_file, 'w') as f:
-        json.dump(filtered_expected, f, indent=4)
-    with open(differences_file, 'w') as f:
-        json.dump(differences, f, indent=4)
-
-    print(f"\nTest failed. Logs written to: {test_log_dir}")
