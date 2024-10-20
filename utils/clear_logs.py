8afc8482f0d0a605a77a469df2932c4660a613bd
alfinalamsyahhh <alfinalamsyahhh@gmail.com>
2024-10-20 07:32:24 +0000

    Add: Clear Logs for Atomic Search

--- utils/clear_logs.py
+++ utils/clear_logs.py
@@ -6,8 +6,11 @@ def clear_logs():
         shutil.rmtree("logs/test_extract_atoms")
     if os.path.exists("logs/test_form_molecule"):
         shutil.rmtree("logs/test_form_molecule")
+    if os.path.exists("logs/test_atomic_search"):
+        shutil.rmtree("logs/test_atomic_search")
     os.makedirs("logs/test_extract_atoms", exist_ok=True)
     os.makedirs("logs/test_form_molecule", exist_ok=True)
+    os.makedirs("logs/test_atomic_search", exist_ok=True)
 
 if __name__ == "__main__":
     clear_logs()
